0c408c4c5d491f2952ef97dda62f23ee2f89f56d
Jón Velasksson <80118997+Ljosilfr@users.noreply.github.com>
2026-04-17 09:59:21 -0400

    Delete wallust/templates/colors.h

--- wallust/templates/colors.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "{{ foreground }}", "{{ background }}" },
-	[SchemeSel] = { "{{ background }}", "{{ color3 }}" },
-	[SchemeOut] = { "{{ background }}", "{{ color3 }}" },
-};
