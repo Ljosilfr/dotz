b7ec0db43ce453c930f6a6ec5fab5a5ac25df75f
Jón Velasksson <80118997+Ljosilfr@users.noreply.github.com>
2026-06-17 17:50:18 -0400

    Delete nvim directory

--- nvim/lua/plugins/haskell-scope-highlighting.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"kiyoon/haskell-scope-highlighting.nvim",
-	init = function()
-	-- Consider disabling other highlighting
-	vim.cmd [[autocmd FileType haskell syntax off]]
-	vim.cmd [[autocmd FileType haskell TSDisable highlight]]
-	end
-}
--- nvim/lua/plugins/live_server.lua
+++
@@ -1,4 +0,0 @@
-return {
-	"barrettruth/live-server.nvim",
-}
-
